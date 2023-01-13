3ca445385979f1050a59444d361ec935f5989dc8
汉璋 涂 <tuhanz20121705@gmail.com>
2023-01-14 02:24:31 +0800

    cmd arg

--- MeshDecomp/decomp.cpp
+++ MeshDecomp/decomp.cpp
@@ -3,14 +3,19 @@
 #include "cu_common_util.cuh"
 #include <thread>
 
-int main()
+
+int main(int argc, char** argv)
 {
+	if (argc < 3)
+		printf("Parameter format: input, output, recursive (optional, 0 for single decomposition, 1 for recursive)\n");
+	bool recursive = true;
+	if (argc > 3 && argv[3][0] == '0') recursive = false;
 	Mesh* mesh = nullptr;
-	mesh = new Mesh("D:/course_proj/MeshDecomp/asset/dino.ply");
+	mesh = new Mesh(argv[1]);
 	mesh->preProcess();
 	mesh->genFuzzyDecomp();
-	mesh->genFinalDecomp(true);
-	mesh->dumpFile("D:/course_proj/MeshDecomp/asset/decomp/dino.ply");
+	mesh->genFinalDecomp(recursive);
+	mesh->dumpFile(argv[2]);
 	return 0;
 }
 
